8c1dcef31d327d64dbcdebc36670a6c5075c6bde
Ashutosh-98765 <asshu65656@gmail.com>
2024-09-11 17:19:53 +0530

    all testcases are done  in this file

---
+++ drill-6/test.js
@@ -0,0 +1,24 @@
+import { counterFactory, limitFunctionCallCount, cacheFunction } from "./closure.js";
+
+// 1:
+
+console.log(counterFactory().increment());
+console.log(counterFactory().decrement());
+
+// 2:
+
+const x = limitFunctionCallCount((data) => data * data, 6);
+
+for(let i=0; i<9; i++){
+    console.log(x(10));
+}
+
+// 3:
+
+const z = cacheFunction((data) => data * 10);
+
+console.log(z(10));
+console.log(z(100));
+console.log(z(10));
+console.log(z(1000));
+console.log(z(10));
