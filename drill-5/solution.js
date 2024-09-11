3952243d57dde34b2a772f5f2dc11a91b1652a44
Ashutosh-98765 <asshu65656@gmail.com>
2024-09-11 16:17:21 +0530

    all testcases are in this file

---
+++ drill-5/solution.js
@@ -0,0 +1,13 @@
+import { testObject, keys, values, mapObject, pairs, invert, defaults } from "./object.js";
+
+console.log(keys(testObject));
+
+console.log(values(testObject));
+
+console.log(mapObject(testObject, (value) => typeof value === 'number' ? value + value : value + " " + value));
+
+console.log(pairs(testObject));
+
+console.log(invert(testObject));
+
+console.log(defaults(testObject, {age: 20, weight: "70 kg", height: "6'0 feet"}));
