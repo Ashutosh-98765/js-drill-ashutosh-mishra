711019e041af75ecee8b0d07cd99ce14d9028a74
Ashutosh-98765 <asshu65656@gmail.com>
2024-09-11 11:49:04 +0530

    added solution.js

---
+++ drill-4/solution.js
@@ -0,0 +1,36 @@
+import { items, each, map, reduce, find, filter, nestedArray, flatten } from "./items.js";
+
+
+// 1:
+
+each(items, (element, index) => {
+    console.log(`${index} : ${element * 2}`);
+});
+
+// 2:
+
+map(items, (element, index) => {
+    console.log(`${index} : ${element * 10}`);
+});
+
+// 3:
+
+console.log(reduce(items, (acc, element) => {
+    return acc *= element;
+}, 1));
+
+// 4:
+
+console.log(find(items, (element) => {
+    return element > 4;
+}));
+
+// 5:
+
+console.log(filter(items, (element) => {
+    return element > 2;
+}));
+
+// 6:
+
+console.log(flatten(nestedArray));
