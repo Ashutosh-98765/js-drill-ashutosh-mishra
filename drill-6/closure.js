f3adcc89f890816d1d4bb32ecea4ed6c50ef7772
Ashutosh-98765 <asshu65656@gmail.com>
2024-09-11 17:18:24 +0530

    all task solution done  in this file

---
+++ drill-6/closure.js
@@ -0,0 +1,31 @@
+export function counterFactory() {
+    let counter = 10;
+    let obj = {
+      increment: () => ++counter,
+      decrement: () => --counter,
+    };
+    return obj;
+  }
+  
+  export function limitFunctionCallCount(cb, n) {
+      let count = 0;
+      return (data) => {
+          if(count < n){
+              count++;
+              return cb(data);
+          }
+          return null;
+      };
+  }
+  
+  export function cacheFunction(cb) {
+      const cache = {};
+      return (key) => {
+          if(cache[key]){        // "cache.hasOwnProperty(key)"
+              return cache[key];
+          }
+          let result = cb(key);
+          cache[key] = result;
+          return result;
+      };
+  }
